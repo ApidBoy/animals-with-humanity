3a16bd9b67a2ece4b10ad664e2f7b6719f21bbec
ApidBoy <samarthsb@protonmail.com>
2022-09-28 05:06:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+app.set('view-engine', 'ejs');
+app.use(express.static(__dirname + '/views/styles'));
+app.use(express.urlencoded({extended:true}));
+app.get('/', (req, res) => {
+    res.render('index.ejs');  
+});
+app.post('/', (req, res) => {
+    console.log(req.body.data);
+});
+app.listen(8000);
