c6e22b8653be6ec748e3440cb443f16c78b9c657
ApidBoy <samarthsb@protonmail.com>
2022-10-09 12:09:57 +0530

    Minor fix

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    res.render('index1.ejs');
+    res.render('index.ejs');
 });
 
 app.post('/', (req, res) => {
